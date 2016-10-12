287e763753a9f0c78c24a8f14f1b7d8bb5550c67
mariogs23 <mario.garcia17@alu.uclm.es>
2016-10-12 17:24:06 +0200

    barra horizontal

---
+++ cliente/js/control-juego.js
@@ -0,0 +1,46 @@
+
+var url="http://127.0.0.1:1338/";
+
+//Funciones que modifican el index
+
+function inicio(){
+	mostrarCabecera();
+}
+
+function borrarControl(){
+	$('#control').remove();
+}
+
+function mostrarCabecera(){
+	$('#cabecera').remove();
+	$('#control').append('<p id="cabecera"><h2>Panel de  Control</h2><input type="text" id="nombre" placeholder="introduce tu nombre"></p>');
+	botonNombre();
+}
+
+function botonNombre(){
+	var nombre="";
+	$('#control').append('<button type="button" id="nombreBtn" class="btn btn-primary btn-md">Iniciar partida</button>');
+	$('#nombreBtn').on('click',function(){
+		nombre=$('#nombre').val();
+		$('#nombre').remove();
+		$('#nombreBtn').remove();		
+		crearUsuario(nombre);
+	});
+}
+
+function mostrarInfoJugador(datos){
+	$('#datos').remove();
+	$('#cabecera').append('<div id="datos">Nombre: '+datos.nombre+' Nivel: '+datos.nivel+' Id:'+datos.id+'</div>');
+}
+
+//Funciones de comunicación con el servidor
+
+function crearUsuario(nombre){
+	if (nombre==""){
+		nombre="jugador";
+	}
+	$.getJSON(url+'crearUsuario/'+nombre,function(datos){
+		mostrarInfoJugador(datos);
+	});
+	//mostrar datos
+}
---
+++ node-express.js
@@ -0,0 +1,33 @@
+
+var fs=require("fs");
+var config=JSON.parse(fs.readFileSync("config.json"));
+var host=config.host;
+var port=config.port;
+var exp=require("express");
+var app=exp(); 
+var modelo=require('./servidor/modelo.js');
+
+var juego= new modelo.Juego();
+
+//app.use(app.router);
+app.use(exp.static(__dirname +"/cliente"));
+
+app.get("/",function(request,response){
+	var contenido=fs.readFileSync("./cliente/index.html");
+	response.setHeader("Content-type","text/html");
+	response.send(contenido);
+});
+
+app.get('/crearUsuario/:nombre',function(request,response){
+	//crear el usuario con el nombre
+	var usuario= new modelo.Usuario(request.params.nombre);
+	juego.agregarUsuario(usuario);
+	var id=usuario.id;
+	usuario=juego.obtenerUsuario(id);
+	console.log(usuario);
+	response.send({'nombre':usuario.nombre,'nivel':usuario.nivel,'id':usuario.id});
+})
+
+console.log("Servidor escuchando en el puerto "+port);
+app.listen(process.env.PORT || 1338);
+
